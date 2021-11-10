72db4aa2d151fab8fa9a155f9eadc0657738b910
seenso <>
2021-11-10 13:04:46 -0900

    Passed test to handle POST req res and append id to DOM (appended to body)

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 // Add your code here
 const submitData = (userName, userEmail) => {
+  //TEST 1 - Send Data
   return fetch("http://localhost:3000/users", {
     method: "POST",
     headers: {
       'Content-Type': "application/json",
       Accept: "application/json"
     },
-    body: JSON.stringify({name: userName, email: userEmail})
-  });
+    body: JSON.stringify({
+      name: userName, 
+      email: userEmail
+    })
+  })
+  //TEST 2 - Handle the Response
+  .then(response => response.json())
+  .then(data => {
+    // data = { id: 132, name: 'Sam', email: 'sam@sam.com' }
+
+    //append id to DOM
+    document.body.append(data.id);
+    // let id = data.id;
+
+  })
+  //TEST 3 - Handle Errors
+  .catch((err) => {console.log("ERROR MESSAGE:", err.message)});
 };
+
