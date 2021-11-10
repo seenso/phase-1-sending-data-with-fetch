5b9d46b89d69b0c41ae260ffca82d586dcc6e6f7
seenso <>
2021-11-10 07:31:12 -0900

    Passed test for 'Takes a POST request to /users with a name and email'

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      'Content-Type': "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({name: userName, email: userEmail})
+  });
+};
